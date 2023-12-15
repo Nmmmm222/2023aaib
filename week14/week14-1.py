576bbef71b3e8cae0115cde8a2f64ac724668a2a
Nmmmm222 <alisawu0606@gmail.com>
2023-12-15 10:07:14 +0800

    week14

---
+++ week14/week14-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        notAns=set()
+        for a,b in paths: #先巡第一輪
+            notAns.add(a) #出發點不是答案
+
+        for a,b in paths: #第二輪
+            if b not in notAns: # b不在出發裡,就是答案
+                return b # b不是[不是答案]
